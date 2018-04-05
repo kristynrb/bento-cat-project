12be71ec3f0e82a3be4995a3fd6c74384eb3d837
Kristyn Bryan <kristynrb@gmail.com>
2018-04-05 14:22:45 -0700

    call to get cat image data

--- feed/feed.js
+++ feed/feed.js
@@ -8,3 +8,19 @@ angular.module('catApp.feed', ['ngRoute'])
     controller: 'FeedCtrl'
   });
 }])
+
+.controller('FeedCtrl', ['$scope', '$http', function($scope, $http) {
+  $scope.catData=[];
+
+  $scope.getCatImages = function(){
+    $http({
+      method: 'GET',
+      url: 'http://thecatapi.com/api/images/get?format=xml&results_per_page=25'
+    })
+    .then(function(response){
+      $scope.catData = response;
+    }, function(response){
+      console.log("ERROR:", response);
+    });
+  }
+}])
