b1f13e9a3606f1f6a54603c48ae501e3696faa19
Kristyn Bryan <kristynrb@gmail.com>
2018-04-06 12:13:23 -0700

    start of routing

--- card/card.js
+++ card/card.js
@@ -3,7 +3,7 @@
 angular.module('catApp.card', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/card', {
+  $routeProvider.when('/card/:cardID', {
     templateUrl: 'card/card.html',
     controller: 'CardCtrl'
   });
